8a81b9ff8c4e9a5cd00c8f9a52fc7858fe8cc469
Kilian Evang <noreply@texttheater.net>
2021-04-21 15:11:22 +0200

    Cleaned up table definition

--- db_structure.sql
+++ db_structure.sql
@@ -7,12 +7,12 @@ DROP TABLE IF EXISTS `vffk`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vffk` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` text NOT NULL,
   `text` text NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `link` varchar(255) NOT NULL,
+  `author` text NOT NULL,
+  `link` varchar(255) CHARACTER SET ascii NOT NULL,
   `updated` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `link` (`link`)
-) ENGINE=MyISAM AUTO_INCREMENT=2691 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
